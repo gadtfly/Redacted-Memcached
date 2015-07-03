418a255ce67694bfda45425e211db8e140d334b4
Adam Louis <adamlouis@gmail.com>
2015-07-02 19:54:10 -0700

    possible chunk failure, and no extra read for size

--- stored_data.rb
+++ stored_data.rb
@@ -19,35 +19,60 @@ class StoredData
       puts 'talking to web'
       data = open(self.url).read
       set_(data)
+      data
     end
-    data
   end
 
 private
+
+  def read_size(chunk)
+    return nil unless chunk
+    size = chunk[-4..-1].to_i
+    chunk[-4..-1] = ""
+    size
+  end
+
+  def write_size(chunk, size)
+    chunk << size.to_s.rjust(4, '0')
+  end
+
+  def append_size(chunks)
+    write_size(chunks.first, chunks.size)
+    chunks
+  end
 
   def chunk_key(n)
     "#{url}#{n}"
   end
 
-  def get_
-    chunks = []
-    n = 0
-    while chunk = client.get(chunk_key(n))
-      n += 1
-      chunks << chunk
-    end
+  def get_chunk(n)
+    chunk = client.get(chunk_key(n))
+    size = n == 0 ? read_size(chunk) : nil
+    {
+      chunk: chunk,
+      size: size
+    }
+  end
 
-    if n == 0
-      nil
-    else
-      chunks.join
+  def get_
+    size = 1
+    n = 0
+    chunks = []
+    while n < size
+      h = get_chunk(n)
+      size = h[:size] if h[:size]
+      chunks << h[:chunk]
+      return nil unless h[:chunk]
+      n += 1
     end
+    chunks.join
   end
 
   def set_(data)
-    chunks_for(data).each_with_index do |chunk, n|
+    size = append_size(chunks_for(data)).each_with_index.map do |chunk, n|
       client.set(chunk_key(n), chunk)
-    end
+    end.size
+    client.set("#{url}size", size)
   end
 
   def chunks_for(data)
